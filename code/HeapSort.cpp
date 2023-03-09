f855066daf2f16d45ff689a8a6d1c30b55c70dca
Cam Nguyen Tran <73975520+cnmeow@users.noreply.github.com>
2023-03-09 21:06:42 +0700

    Create HeapSort.cpp

---
+++ code/HeapSort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+const int n = 1000000;
+const string filename = "test10.inp";
+int a[1000005];
+void heapify(int N, int i) {
+    int largest = i;
+    int l = 2 * i + 1, r = 2 * i + 2;
+    if (l < N && a[l] > a[largest]) largest = l;
+    if (r < N && a[r] > a[largest]) largest = r;
+    if (largest != i) {
+        swap(a[i], a[largest]);
+        heapify(N, largest);
+    }
+}
+void heapSort() {
+    for (int i = n/2 - 1; i >= 0; --i) heapify(n, i);
+    for (int i = n-1; i >= 0; --i) {
+        swap(a[0], a[i]);
+        heapify(i, 0);
+    }
+}
+int main() {
+    auto starttime = high_resolution_clock::now();
+    ifstream fi(filename);
+    for (int i = 0; i < n; ++i) fi >> a[i];
+    heapSort();
+    auto finishtime = high_resolution_clock::now();
+    cout << duration_cast<microseconds>(finishtime - starttime).count();
+    return 0;
+}
